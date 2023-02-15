4a37c4f1f316599142f8e0cb8e5abc6939586dda
Diogo Neves <diogo.neves@gmail.com>
2023-02-15 22:05:47 +0000

    works

--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,44 @@
-var y: i32 = add(10, x);
-const x: i32 = add(12, 34);
+//
+// basic_window
+// Zig version: 0.6.0
+// Author: Nikolas Wipper
+// Date: 2020-02-15
+//
 
-test "global variables" {
-    assert(x == 46);
-    assert(y == 56);
-}
+const rl = @import("raylib");
 
-fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+pub fn main() anyerror!void {
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const screenWidth = 800;
+    const screenHeight = 450;
 
-const std = @import("std");
-const assert = std.debug.assert;
+    rl.InitWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
 
-pub fn main() !void {
-    std.debug.print("Testing", .{});
+    rl.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!rl.WindowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        rl.BeginDrawing();
+
+        rl.ClearBackground(rl.WHITE);
+
+        rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LIGHTGRAY);
+
+        rl.EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    rl.CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 }
