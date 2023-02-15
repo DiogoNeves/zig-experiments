b574be22b15be540d602b60ceebc69184594adf6
Diogo Neves <diogo.neves@gmail.com>
2023-02-15 23:16:33 +0000

    fixed project name

--- build.zig
+++ build.zig
@@ -2,14 +2,13 @@ const std = @import("std");
 const Builder = std.build.Builder;
 const raylib = @import("raylib-zig/lib.zig"); //call .Pkg() with the folder raylib-zig is in relative to project build.zig
 
-
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const target = b.standardTargetOptions(.{});
 
     const system_lib = b.option(bool, "system-raylib", "link to preinstalled raylib libraries") orelse false;
 
-    const exe = b.addExecutable("testing", "src/main.zig");
+    const exe = b.addExecutable("hello-world", "src/main.zig");
     exe.setBuildMode(mode);
     exe.setTarget(target);
 
@@ -18,9 +17,8 @@ pub fn build(b: *Builder) void {
     raylib.math.addAsPackage("raylib-math", exe);
 
     const run_cmd = exe.run();
-    const run_step = b.step("run", "run testing");
+    const run_step = b.step("run", "run hello-world");
     run_step.dependOn(&run_cmd.step);
 
     exe.install();
 }
-
